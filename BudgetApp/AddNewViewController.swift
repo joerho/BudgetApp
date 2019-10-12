42f37eed26684bd3769f8fbb0b3bf97d0ab6ceac
joerho1@gmail.com <joerho1@gmail.com>
2019-10-11 20:49:34 -0700

    added cancel action

--- BudgetApp/AddNewViewController.swift
+++ BudgetApp/AddNewViewController.swift
@@ -20,23 +20,30 @@ class AddNewViewController: LBTAFormController {
         
         let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 66))
         let navItem = UINavigationItem(title: "Add New")
-        let rightButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: nil)
+        let rightButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(closeView))
         navItem.rightBarButtonItem = rightButton
         navBar.items = [navItem]
         self.view.addSubview(navBar)
         
         formContainerStackView.axis = .vertical
-        formContainerStackView.spacing = 12
+        formContainerStackView.spacing = 0
         formContainerStackView.layoutMargins = .init(top: 50, left: 24, bottom: 0, right: 24)
         
+        
         (0...3).forEach {(_) in
-            let tf = IndentedTextField(placeholder: "enter text", padding: 12, cornerRadius: 5,  backgroundColor: UIColor(named: "TextFieldColor") ?? .white)
+            //let tf = IndentedTextField(placeholder: "enter text", padding: 12, cornerRadius: 5,  backgroundColor: UIColor(named: "TextFieldColor") ?? .white)
+            let tf = UITextField(placeholder: "enter text")
+            tf.clearButtonMode = UITextField.ViewMode.whileEditing
+            tf.backgroundColor = UIColor(named: "TextFieldColor")
 
             tf.constrainHeight(50)
-
             formContainerStackView.addArrangedSubview(tf)
             
         }
+    }
+    
+    @objc func closeView() {
+        dismiss(animated: true, completion: nil)
     }
 }
 
